67230067f504f788368adfdb0d319fb91c0eab9b
Shilpika S <sshilpika5@gmail.com>
2016-06-03 22:36:23 -0500

    recompute the metrics results

--- src/main/scala/TestObj.scala
+++ src/main/scala/TestObj.scala
@@ -4,6 +4,8 @@ import akka.util.Timeout
 import org.apache.spark.{SparkConf, SparkContext}
 import concurrent.duration._
 import concurrent.ExecutionContext.Implicits._
+import scala.concurrent._
+
 /**
  * Created by shilpika on 7/24/15.
  */
@@ -16,9 +18,17 @@ object TestObj {
 
     println(s"You entered: \nUsername: ${input(0)} \nReponame: ${input(1)} \nBranchname: ${input(2)}\n")
     val timeout = Timeout(1 hour)
-    CommitDensityService.dataForMetrics(input(0), input(1), input(2), "week")
-    CommitDensityService.dataForMetrics(input(0), input(1), input(2), "month")
-    CommitDensityService.storeRepoName(input(0)+"_"+input(1)+"_"+input(2))
+    val user = input(0)
+    val repo = input(1)
+    val branch = input(2)
+    CommitDensityService.dataForMetrics(user, repo, branch, "week")
+    CommitDensityService.dataForMetrics(user, repo, branch, "month")
+    log.info("DONE storing commit details and defect density result for "+repo)
+    // store db names for tracked dbs
+    //log.info("Storing tracked Db name for "+repo)
+    //CommitDensityService.storeRepoName(user+"_"+repo+"_"+branch)
+    val res = Future("Final Metrics results stored in DB")
+    Await.result(res,5 minutes)
     println("done")
 
   }
